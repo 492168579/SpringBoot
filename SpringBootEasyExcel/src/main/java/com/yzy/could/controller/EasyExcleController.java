abafbada088ea1a493f35d790a4b142c4af6750e
492168579 <492168579@qq.com>
2020-09-10 11:23:04 +0800

    代码提交

---
+++ SpringBootEasyExcel/src/main/java/com/yzy/could/controller/EasyExcleController.java
@@ -0,0 +1,31 @@
+package com.yzy.could.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.yzy.could.entity.User;
+
+@RestController
+public class EasyExcleController {
+
+	/**
+	 * 文件上传
+	 * <p>
+	 * 1. 创建excel对应的实体对象 参照{@link UploadData}
+	 * <p>
+	 * 2. 由于默认一行行的读取excel，所以需要创建excel一行一行的回调监听器，参照{@link UploadDataListener}
+	 * <p>
+	 * 3. 直接读即可
+	 */
+	@PostMapping("upload")
+	@ResponseBody
+	public String upload(MultipartFile file) throws Exception {
+		EasyExcel.read(file.getInputStream(), UploadData.class, new UploadDataListener(uploadDAO)).sheet().doRead();
+		return "success";
+	}
+}
--- SpringBootUFLO2/README.md
+++ SpringBootUFLO2/README.md
@@ -1,3 +1,10 @@
 # SpringBootUFLO2
 ## UFLO2 简介： UFLO是锐道自主研发的一款开源的工作流引擎，采用Apache-2.0开源协议
 官方文档地址：[github地址](https://github.com/youseries/uflo)               [在线文档教程](http://wiki.bsdn.org/display/UFLO)
+### 1 网页的流程模版设计器
+ 打开浏览器， 浏览[网页的流程模版设计器](http://localhost:8080/SpringBootUFLO2/uflo/designer)
+### 2 任务管理
+[任务管理](http://localhost:8080/SpringBootUFLO2/uflo/todo) 
+### 3 流程监控与测试
+[流程监控与测试](http://localhost:8080/SpringBootUFLO2/uflo/central) 
+ 
